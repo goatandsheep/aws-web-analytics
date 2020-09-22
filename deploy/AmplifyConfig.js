7515825802995612892cff04458e0a332292c6fd
Jack Markiewicz <jack.solutionbox@gmail.com>
2020-09-22 10:41:59 -0700

    Typescript update

---
+++ deploy/AmplifyConfig.js
@@ -0,0 +1,19 @@
+import Amplify from '@aws-amplify/core';
+export default class AmplifyConfig {
+    constructor(pinpointId, fedpool, region = 'us-east-1') {
+        Amplify
+            .configure({
+            Auth: {
+                region,
+                identityPoolId: fedpool
+            },
+            Analytics: {
+                AWSPinpoint: {
+                    appId: pinpointId,
+                    region: region,
+                    mandatorySignIn: false
+                }
+            }
+        });
+    }
+}
